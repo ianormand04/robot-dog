386aeb0297844ab5bd0fff9ebfd85b9b149ad8f6
Ian Ormand <ian.ormand2004@gmail.com>
2025-12-09 19:36:27 -0700

    Create moving_functions.py

---
+++ moving_functions.py
@@ -0,0 +1,13 @@
+#Just assume that these are working properly for now
+
+def move_forward(){
+  #moves the robot forward
+}
+
+def turn_right(degrees){
+  #turns robot right by ___ degrees
+}
+
+def move_left(degrees){
+  #turns robot left by __ degrees
+}
