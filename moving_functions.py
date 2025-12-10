f5072d56079d53a6fb29aec26c142e2d67701acc
Ian Ormand <ian.ormand2004@gmail.com>
2025-12-09 20:37:48 -0700

    created empty moving functions

--- moving_functions.py
+++ moving_functions.py
@@ -1,13 +1,13 @@
 #Just assume that these are working properly for now
 
-def move_forward(){
+def move_forward():
   #moves the robot forward
-}
+  pass
 
-def turn_right(degrees){
+def turn_right(degrees):
   #turns robot right by ___ degrees
-}
+  pass
 
-def move_left(degrees){
+def move_left(degrees):
   #turns robot left by __ degrees
-}
+  pass
